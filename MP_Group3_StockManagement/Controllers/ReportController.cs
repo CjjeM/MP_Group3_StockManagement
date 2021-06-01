33d0d88550a0486f90f99aa441524ead34a525af
jobert <EpicLegitPlayer@gmail.com>
2021-06-01 19:22:32 +0800

    Added Report, Bug fixes

---
+++ MP_Group3_StockManagement/Controllers/ReportController.cs
@@ -0,0 +1,73 @@
+﻿using MP_Group3_StockManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MP_Group3_StockManagement.Controllers
+{
+    public class ReportController : Controller
+    {
+        MP_StockManagementContext db = new MP_StockManagementContext();
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        public ActionResult GetBelowCritical()
+        {
+            var model = from p in db.Products
+                        where p.SafetyLevel == 1
+                        select p;
+            return PartialView("BelowCritical", model.ToList());
+        }
+        public ActionResult GetWithinCritical()
+        {
+            var model = from p in db.Products
+                        where p.SafetyLevel == 2
+                        select p;
+            return PartialView("WithinCritical", model.ToList());
+        }
+        public ActionResult GetAboveCritical()
+        {
+            var model = from p in db.Products
+                        where p.SafetyLevel == 3
+                        select p;
+            return PartialView("AboveCritical", model.ToList());
+        }
+
+        public ActionResult GetExpiredProductsList()
+        {
+            var model = from p in db.Products
+                        where p.ExpirationDate != null &&
+                        DateTime.Compare(DateTime.Now, (DateTime)p.ExpirationDate) > 0
+                        select p;
+            return PartialView("ExpiredProductsList", model.ToList());
+        }
+
+        public ActionResult GetCostCurrentAvailableStocks()
+        {
+            var model = from p in db.Products
+                        where p.ExpirationDate == null ||
+                        DateTime.Compare(DateTime.Now, (DateTime)p.ExpirationDate) < 1
+                        select p.ProductPrice;
+
+            ViewBag.CurrentStocksCost = Queryable.Sum(model);
+
+            return PartialView("CostCurrentAvailableStocks");
+        }
+        public ActionResult GetCostExpiredStocks()
+        {
+            var model = from p in db.Products
+                        where p.ExpirationDate != null &&
+                        DateTime.Compare(DateTime.Now, (DateTime)p.ExpirationDate) > 0
+                        select p.ProductPrice;
+
+            ViewBag.CurrentExpiredCost = Queryable.Sum(model);
+
+            return PartialView("CostExpiredStocks");
+        }
+    }
+}
